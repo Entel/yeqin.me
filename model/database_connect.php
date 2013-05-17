aa56754054f5034638c45f94c5ceb1ad7a383a2d
Entel <entel.science@gmail.com>
2013-05-17 21:37:05 +0800

    05172135

--- model/database_connect.php
+++ model/database_connect.php
@@ -1,6 +1,6 @@
 <?php
 	header("Content-Type: text/html; charset=utf-8");
-	$dbc = mysqli_connect('localhost','root','12341234','blog')
+	$dbc = mysqli_connect('localhost','root','12341234','yeqin')
 		or die('Error connecting to MySQL serves');
 	mysql_query("SET NAMES 'utf8'");
         mysql_query("set character set 'utf8'");
